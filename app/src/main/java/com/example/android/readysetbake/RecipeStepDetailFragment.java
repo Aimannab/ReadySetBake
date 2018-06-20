8d7de341caadf47f9013498ae8d932a783fe20d7
Aiman <aiman.nabeel@gmail.com>
2018-06-20 14:48:40 +0100

    Setting up ExoPlayer

--- app/src/main/java/com/example/android/readysetbake/RecipeStepDetailFragment.java
+++ app/src/main/java/com/example/android/readysetbake/RecipeStepDetailFragment.java
@@ -1,14 +1,20 @@
 package com.example.android.readysetbake;
 
+import android.content.Context;
+import android.content.res.Configuration;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.exoplayer2.SimpleExoPlayer;
 import com.google.android.exoplayer2.ui.AspectRatioFrameLayout;
@@ -47,10 +53,19 @@ public class RecipeStepDetailFragment extends Fragment {
         void onRecipeStepDetailItemClick(List<Step> stepsOut, int itemSelectedIndex, String recipeName);
     }
 
+    //Method initializePlayer to implement in onCreateView
+    private void initializePlayer(Uri mediaUri) {
+        //pendinggggggggg
+    }
+
+    //Method isInLandscapeMode to implement in onCreateView
+    public boolean isInLandscapeMode (Context context) {
+        return (context.getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE);
+    }
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-
         recipeList = new ArrayList<>();
 
         clickListener =(RecipeDetailActivity)getActivity();
@@ -87,17 +102,74 @@ public class RecipeStepDetailFragment extends Fragment {
         exoPlayerView = (SimpleExoPlayerView) rootView.findViewById(R.id.exoPlayerView);
         exoPlayerView.setResizeMode(AspectRatioFrameLayout.RESIZE_MODE_FIT);
 
+        //Getting video URL
+        String videoUrl = recipeSteps.get(selectedIndex).getVideoURL();
+
+        //Inflating view for phone or iPad
         if (rootView.findViewWithTag("sw600dp-port-recipe_step_detail")!=null) {
             recipeName=((RecipeDetailActivity) getActivity()).recipeName;
             ((RecipeDetailActivity) getActivity()).getSupportActionBar().setTitle(recipeName);
         }
 
+        //Getting imageUrl
         String imageUrl=recipeSteps.get(selectedIndex).getThumbnailURL();
         if (imageUrl!="") {
             Uri builtUri = Uri.parse(imageUrl).buildUpon().build();
             ImageView thumbImage = (ImageView) rootView.findViewById(R.id.thumbImage);
             Picasso.with(getContext()).load(builtUri).into(thumbImage);
         }
+
+        //Using initializePlayer method here
+        if (!videoUrl.isEmpty()) {
+            initializePlayer(Uri.parse(recipeSteps.get(selectedIndex).getVideoURL()));
+
+            if (rootView.findViewWithTag("sw600dp-land-recipe_step_detail")!=null) {
+                getActivity().findViewById(R.id.recipe_fragment_container2).setLayoutParams(new LinearLayout.LayoutParams(-1,-2));
+                exoPlayerView.setResizeMode(AspectRatioFrameLayout.RESIZE_MODE_FIXED_WIDTH);
+            }
+            else if (isInLandscapeMode(getContext())){
+                recipeStepDetailTextview.setVisibility(View.GONE);
+            }
+        }
+        else {
+            exoPlayer=null;
+            //exoPlayerView.setForeground(ContextCompat.getDrawable(getContext(), R.drawable.ic_visibility_off_white_36dp));
+            exoPlayerView.setLayoutParams(new LinearLayout.LayoutParams(300, 300));
+        }
+
+        //Setting up previous and next step buttons for recipe steps, using onClickListener
+        Button recipePrevStep = (Button) rootView.findViewById(R.id.previousStep);
+        Button recipeNextstep = (Button) rootView.findViewById(R.id.nextStep);
+
+        recipePrevStep.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                if (recipeSteps.get(selectedIndex).getId() > 0) {
+                    if (exoPlayer!=null){
+                        exoPlayer.stop();
+                    }
+                    clickListener.onRecipeStepDetailItemClick(recipeSteps,recipeSteps.get(selectedIndex).getId() - 1,recipeName);
+                }
+                else {
+                    Toast.makeText(getActivity(),"You already are in the First step of the recipe", Toast.LENGTH_SHORT).show();
+
+                }
+            }});
+
+        recipeNextstep.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+
+                int lastIndex = recipeSteps.size()-1;
+                if (recipeSteps.get(selectedIndex).getId() < recipeSteps.get(lastIndex).getId()) {
+                    if (exoPlayer!=null){
+                        exoPlayer.stop();
+                    }
+                    clickListener.onRecipeStepDetailItemClick(recipeSteps,recipeSteps.get(selectedIndex).getId() + 1,recipeName);
+                }
+                else {
+                    Toast.makeText(getContext(),"You already are in the Last step of the recipe", Toast.LENGTH_SHORT).show();
+
+                }
+            }});
 
         return rootView;
     }
@@ -108,5 +180,25 @@ public class RecipeStepDetailFragment extends Fragment {
         currentState.putParcelableArrayList(SELECTED_STEPS,recipeSteps);
         currentState.putInt(SELECTED_INDEX,selectedIndex);
         currentState.putString("Title",recipeName);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
     }
 }
