4ff250c05a03a6f6b7c1241237e46e108c38328e
Aiman <aiman.nabeel@gmail.com>
2018-07-12 11:50:51 +0100

    Setting up Widget

---
+++ app/src/androidTest/java/com/example/android/readysetbake/RecipeButtonClickTest.java
@@ -0,0 +1,19 @@
+package com.example.android.readysetbake;
+
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.runner.RunWith;
+//import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+/**
+ * Created by Aiman Nabeel on 11/07/2018.
+ */
+
+@RunWith(AndroidJUnit4.class)
+public class RecipeButtonClickTest {
+
+    //@Rule
+    //public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+}
