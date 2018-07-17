1471500d57c4011021143d78e59b2b2d3682cf2a
Aiman <aiman.nabeel@gmail.com>
2018-07-17 12:16:56 +0100

    Setting up widget

--- app/src/main/java/com/example/android/readysetbake/ReadySetBakeWidgetProvider.java
+++ app/src/main/java/com/example/android/readysetbake/ReadySetBakeWidgetProvider.java
@@ -52,14 +52,15 @@ public class ReadySetBakeWidgetProvider extends AppWidgetProvider {
         appWidgetManager.updateAppWidget(appWidgetId, views);
     }
 
-    //Won't be using this method because appWidgetIds[] is required, instead using custom method onUpdateBakingWidgets here
+    //Creating a custom method onUpdateBakingWidgets and calling it's updateAppWidget() here
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         // There may be multiple widgets active, so update all of them
 
-        // for (int appWidgetId : appWidgetIds) {
-        //updateAppWidget(context, appWidgetManager, ingredientsList, appWidgetId);
-        // }
+         for (int appWidgetId : appWidgetIds) {
+        updateAppWidget(context, appWidgetManager, appWidgetId);
+         }
+
     }
 
     public static void onUpdateBakingWidgets(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
